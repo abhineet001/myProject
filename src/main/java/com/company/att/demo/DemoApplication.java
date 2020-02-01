90ec5dbd4b68eb090993a7f8e5248d34991b09c6
abhineet001 <59792284+abhineet001@users.noreply.github.com>
2020-02-02 00:42:32 +0530

    Commit

--- src/main/java/com/company/att/demo/DemoApplication.java
+++ src/main/java/com/company/att/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.company.att.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*This is my first class*/
+
 @SpringBootApplication
 public class DemoApplication {
 
